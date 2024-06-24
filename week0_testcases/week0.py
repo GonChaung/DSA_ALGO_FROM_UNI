435e634996855d648692e28a36e0d15b91df5798
Pandas-12 <thantsithu.naing@icloud.com>
2024-06-24 10:02:02 +0700

    inss

--- week0_testcases/week0.py
+++ week0_testcases/week0.py
@@ -4,3 +4,11 @@ def print_numbers(n):
 
 n = int(input("How many integers do you want to input : "))
 print_numbers(n)
+
+# Insertion sort
+# for i in range(1, len(a)):
+#     j = i
+#     while j > 0 and a[j - 1] > a[j]:
+#         # Swap
+#         a[j], a[j - 1] = a[j - 1], a[j]
+#         j -= 1
--- week2_work&test/inssort.py
+++ week2_work&test/inssort.py
@@ -1,17 +1,14 @@
-
 import time
-
 a = list(map(int, input().split()))
-
 n = len(a)
-
 st = time.process_time()
 
 # write the insertion sort code into this segment
-
-
-    
+for i in range(1,n):
+    j = i
+    while j > 0 and a[j-1] > a[j]:
+        a[j], a[j-1] = a[j-1], a[j]
+        j -= 1
 et = time.process_time()
-
 print(a)
 print(et-st)
