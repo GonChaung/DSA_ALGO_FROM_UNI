e09d97bb2fbcceddc8143295febb04ad1f54c810
Pandas-12 <thantsithu.naing@icloud.com>
2024-07-23 15:12:22 +0700

    pwManagnment

---
+++ Week 5/Pw.py
@@ -0,0 +1,8 @@
+import getpass
+
+user_name = input("Enter your username: ")
+user_password = getpass.getpass("Enter your password: ")
+
+print("Username:", user_name)
+print("Password entered securely.")
+
