8681196e84251e0eff7eb2ab6757f872ee1b68cd
Pandas-12 <thantsithu.naing@icloud.com>
2024-08-26 09:19:19 +0700

    week8

---
+++ week8_/postfixEval0.py
@@ -0,0 +1,20 @@
+
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+Stack = []
+postfix_expr = input().split()
+for token in postfix_expr:
+    if is_number(token):
+        # Fill in code here for token being a number
+    else:
+        # Fill in code here for token not being a number
+        
+print('%.1f' % Stack[0])
+
+
+        
